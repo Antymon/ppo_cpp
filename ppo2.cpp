b7108decf855aa2191d89c51dfcdedfb44221438
szymon <szymon.brych@gmail.com>
2019-07-15 14:23:12 +0100

    tensor board separate dirs for logs

--- ppo2.cpp
+++ ppo2.cpp
@@ -9,12 +9,18 @@
 
 int main(){
 
+    auto seconds = time (nullptr);
+
+    std::string tb_path {"./exp/ppo_cpp/tensorboard/ppo_"+std::to_string(seconds)+"/"};
+    std::string mkdir_sys_call {"mkdir -p "+tb_path};
+    system(mkdir_sys_call.c_str());
+
     CartPoleEnv e {1};
     PPO2 algorithm {"./exp/ppo_cpp/ppo2_graph_forced_cont_actions.meta.txt",e,
-                    0.99,2048,0,1e-3,0.5f,.5,.95,32,10,0.2,-1,"exp/ppo_cpp/tensorboard/"
+                    0.99,2048,0,1e-3,0.5f,.5,.95,32,10,0.2,-1,tb_path
     };
 
-    algorithm.learn(2e7);
+    algorithm.learn(static_cast<int>(2e7));
 
     return 0;
 }
