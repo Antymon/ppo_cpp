3f1fca63e8b3209af14faaced169c753458fdcfb
szymon <szymon.brych@gmail.com>
2019-09-05 09:11:41 +0100

    Simg help

--- singularity/help.sh
+++ singularity/help.sh
@@ -1,8 +1,8 @@
 echo
 echo "USAGE:"
 echo
-echo "  *.simg RUN_ID CONFIGURATION_ID GRAPH_PATH [OPTIONS]"
+echo "  *.simg RUN_ID CONFIGURATION_ID GRAPH_PATH {OPTIONS}"
 echo
-echo "As a proxy to following executable: "
+echo "As a proxy to the following executable: "
 echo
 /git/sferes2/build/exp/ppo_cpp/ppo_cpp --help
